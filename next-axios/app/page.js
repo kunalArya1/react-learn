d79a3b4e492cd67df456c9d8da97dbd7bbc6b428
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-07-07 15:34:50 +0530

    fetching data using axios promises

--- next-axios/app/page.js
+++ next-axios/app/page.js
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Homepage = () => {
+
+  // creating state to store data
+  const [data, setdata] = useState([]);
+
+
+  // fetching the data using Axios
+  const alldata = () => {
+    axios
+      .get("https://picsum.photos/v2/list?page=2&limit=21")
+      .then((res) => {
+        // console.log(res.data);
+        setdata(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    alldata();
+  }, []);
+
+
+  // runnig map function to get all data
+  const image = data.map((image, i) => {
+    return (
+      <div key={image.id} className="image-div">
+        <img src={image.download_url} height={200} alt="" />
+        <p>{image.author}</p>
+      </div>
+    );
+  });
+
+  // rendering the data
   return (
-    <div>Homepage</div>
-  )
-}
+    <div>
+      <h1 className="title">AXIOS WORKING</h1>
+      <hr />
+      <div className="grid">{image}</div>
+    </div>
+  );
+};
 
 export default Homepage;
