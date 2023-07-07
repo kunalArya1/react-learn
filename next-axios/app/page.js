446eb5feabe771ee3d86e56672fccc4bd947ec1e
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-07-07 15:46:54 +0530

    updated

--- next-axios/app/page.js
+++ next-axios/app/page.js
@@ -6,18 +6,20 @@ const Homepage = () => {
 
   // creating state to store data
   const [data, setdata] = useState([]);
+  const [error, seterror] = useState()
 
 
   // fetching the data using Axios
   const alldata = () => {
     axios
-      .get("https://picsum.photos/v2/list?page=2&limit=21")
+      .get("https://picsum.photos/v2/list?page=18&limit=18")
       .then((res) => {
         // console.log(res.data);
         setdata(res.data);
       })
       .catch((err) => {
         console.log(err);
+        seterror(err);
       });
   };
 
@@ -39,7 +41,8 @@ const Homepage = () => {
   // rendering the data
   return (
     <div>
-      <h1 className="title">AXIOS WORKING</h1>
+      <h1 className="title">AXIOS </h1>
+      {error!== "" && <h2>{error}</h2>}
       <hr />
       <div className="grid">{image}</div>
     </div>
